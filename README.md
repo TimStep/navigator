00233927c93382a7689b081518366ec246855bf0
TimStep <tim060512@gmail.com>
2023-12-04 18:55:59 +0300

    Initial commit. Added main files and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is aimed at creating a visualization of finding differnt paths in graphs
---
+++ graph_visualisation.py
@@ -0,0 +1,63 @@
+#здесь должен быть код визуализирующий граф с весом для каждого пути и отдельно должна быть функция для визуализирования кратчайшего пути
+#возможно следует продумать систему передачи узлам графа координат( или декартовых или полярных), но пока я не понял как это можно адекватно сделать
+#также следует разобраться с библиотекой networkx, которая графы визуализирует, но делает это в рандомном порядке, что возможно не является проблемой
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+# Defining a Class
+class GraphVisualization:
+
+    def __init__(self):
+
+        self.visual = []
+
+
+
+
+    def addEdge(self, a, b,c):
+        a = [a, b]
+        self.visual.append(a)
+
+
+    def visualize(self):
+        G = nx.Graph()
+        G.add_edges_from(self.visual)
+        nx.draw_networkx(G)
+        plt.show()
+
+
+
+
+G = GraphVisualization()
+roads = [
+    ('X', 'A', 7),
+    ('X', 'B', 2),
+    ('X', 'C', 3),
+    ('X', 'E', 4),
+    ('A', 'B', 3),
+    ('A', 'D', 4),
+    ('B', 'D', 4),
+    ('B', 'H', 5),
+    ('C', 'L', 2),
+    ('D', 'F', 1),
+    ('F', 'H', 3),
+    ('G', 'H', 2),
+    ('G', 'Y', 2),
+    ('I', 'J', 6),
+    ('I', 'K', 4),
+    ('I', 'L', 4),
+    ('J', 'L', 1),
+    ('K', 'Y', 5),
+]
+
+
+
+
+
+
+for road in roads:
+    G.addEdge(*road)
+G.visualize()
