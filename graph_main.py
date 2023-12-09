f52331d3fa6dd2ff245c07cd4936a64f709817ff
TimStep <tim060512@gmail.com>
2023-12-09 10:43:21 +0300

    updated graph_main.py: added QOL for debgugging and testing

--- graph_main.py
+++ graph_main.py
@@ -3,12 +3,14 @@ INF = math.inf
 
 #IMPORTANT: nodes are indexed by numbers starting from zero, NOT names
 #If nodes are not connetcted the weight is INF
+#In case the graph is oriented the logic is: A[from_node][to_node]
 test_graph = [
              [0, 1, 1, 1, INF],
              [1, 0, INF, 1, 1], 
              [1, INF, 0, 1, INF], 
              [1, 1, 1, 0, 1], 
              [INF, 1, INF, 1, 0]]
+#test graph: https://u.foxford.ngcdn.ru/uploads/tinymce_image/image/1959/1.png
 
 class Graph():
 	
@@ -16,13 +18,14 @@ class Graph():
 		#takes the initialization matrix
 		self.matr = matr
 		self.size = len(self.matr)
+		#self.edges = 
 		
 	def dijkstra(self, start, end):
 		#array of distances from the start node to other nodes
 		dist_from_start = [INF]*self.size
 		dist_from_start[start] = 0
 		
-		#remembers the path to each as arrays
+		#remembers the path to each node as arrays
 		path_from_start = [[] for i in range(self.size)]
 		path_from_start[start] = [start]
 		#list of visited nodes, non-ordered
@@ -34,8 +37,8 @@ class Graph():
 			
 			#node relaxation
 			for next_node in range(self.size):
-				if self.matr[next_node][curr_node] + dist_from_start[curr_node] < dist_from_start[next_node]:
-					dist_from_start[next_node] = self.matr[next_node][curr_node] + dist_from_start[curr_node]
+				if self.matr[curr_node][next_node] + dist_from_start[curr_node] < dist_from_start[next_node]:
+					dist_from_start[next_node] = self.matr[curr_node][next_node] + dist_from_start[curr_node]
 					path_from_start[next_node] = path_from_start[curr_node] + [next_node]
 			visited.append(curr_node)
 			
@@ -54,4 +57,4 @@ class Graph():
 g = Graph(test_graph)
 #print(g.dijkstra(3, 4))
 #print(g.dijkstra(4, 5))
-print(g.dijkstra(2, 4))
+print("Shortest path from node 2 to node 4: " + '->'.join(str(node) for node in g.dijkstra(2, 4)))
