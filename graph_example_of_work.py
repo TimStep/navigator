e0d4e993358701166ee83a849bf79e587c04938e
Vladimir Hromyka <151759853+Void1977@users.noreply.github.com>
2023-12-20 13:31:27 +0300

    Update graph_example_of_work.py

--- graph_example_of_work.py
+++ graph_example_of_work.py
@@ -18,7 +18,7 @@ canvas_widget.create_window(150, 160, window=label_widget1)
 label_widget2 = Label(app, text="Enter finish")
 canvas_widget.create_window(150, 200, window=label_widget2)
 
-label_widget3 = Label(app, text="Enter N")
+label_widget3 = Label(app, text="Enter size of matrix")
 canvas_widget.create_window(150, 240, window=label_widget3)
 
 entry_widget1 = Entry(app)
@@ -36,15 +36,14 @@ canvas_widget.create_window(300, 240, window=entry_widget3)
 b = graph_matrix()
 
 
-def entry_data():
 
-
-    start = int(float(entry_widget1.get()))
-    finish = int(float(entry_widget2.get()))
-    return start,finish
 def get_n():
-    n=int(float(entry_widget3.get()))
-    return  n
+    if entry_widget3.get() == '':
+        print('Please, enter the size of matrix!')
+
+    else:
+        n=int(float(entry_widget3.get()))
+        return  n
 def get_matr():
     fig = Figure(figsize=(5, 5),
                  dpi=100)
@@ -56,14 +55,27 @@ def plot_graph():
 
     a = Visualisate(b.matr)
     a.vis_simple_graph()
-def plot_shortest_way():
-
-
-    start,finish = entry_data()
+def plot_shortest_way_FB():
+    start = int(float(entry_widget1.get()))
+    finish = int(float(entry_widget2.get()))
 
 
     a = Visualisate(b.matr)
-    a.vis_shortest_way(start,finish)
+    a.vis_shortest_way_FB(start,finish)
+def plot_shortest_way_D():
+    start = int(float(entry_widget1.get()))
+    finish = int(float(entry_widget2.get()))
+
+
+    a = Visualisate(b.matr)
+    a.vis_shortest_way_D(start,finish)
+def plot_shortest_way_FW():
+    start = int(float(entry_widget1.get()))
+    finish = int(float(entry_widget2.get()))
+
+
+    a = Visualisate(b.matr)
+    a.vis_shortest_way_FW(start,finish)
 
 
 
@@ -88,16 +100,28 @@ plot_button = Button(master=app,
                      text="Plot graph")
 
 plot_button_way = Button(master=app,
-                     command=plot_shortest_way,
+                     command=plot_shortest_way_D,
                      height=2,
-                     width=20,
-                     text="Plot shortest way")
+                     width=30,
+                     text="Plot shortest way with D algoritm")
+plot_button_way_2 = Button(master=app,
+                     command=plot_shortest_way_FW,
+                     height=2,
+                     width=30,
+                     text="Plot shortest way with FW algoritm")
+plot_button_way_3 = Button(master=app,
+                     command=plot_shortest_way_FB,
+                     height=2,
+                     width=30,
+                     text="Plot shortest way with FB algoritm")
 
 
 # place the button
 # in main window
 plot_button.pack()
 plot_button_way.pack()
+plot_button_way_2.pack()
+plot_button_way_3.pack()
 button_matr.pack()
 # run the gui
 app.mainloop()
