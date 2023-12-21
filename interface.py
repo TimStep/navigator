246bcb126a6e6b1d96481e721c14162d13a2d4f3
Nikolai-Znamenskii <153112504+Nikolai-Znamenskii@users.noreply.github.com>
2023-12-21 14:03:52 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,11 +6,10 @@ INF = math.inf
 ct.set_appearance_mode('dark')
 ct.set_default_color_theme('dark-blue')
 root = ct.CTk()
-root.geometry('700x1000')
+root.geometry('700x1200')
 root.title('NAVIGATOR')
-First_label = ct.CTkLabel(root, text='''Hello!!! This is Graph Navigator!!!
-Please, add your graph''')
-First_label.pack(pady=40)
+First_label = ct.CTkLabel(root, text='''Please, add your graph''')
+First_label.pack(pady=10)
 
 
 functionlabel = ct.CTkLabel(root, text='write function')
@@ -20,7 +19,7 @@ entryoffunction.pack(pady=20)
 
 
 
-graphlabel = ct.CTkLabel(root, text='write graph matrix or massive of edges')
+graphlabel = ct.CTkLabel(root, text='write graph matrix or massive of edges (if second, format must be: "1,2,12 2,3,22 name1,name2,length  ..."')
 entryofgraph = ct.CTkEntry(root)
 graphlabel.pack(pady=10)
 entryofgraph.pack(pady=20)
@@ -34,28 +33,69 @@ entryoftype.pack(pady=20)
 
 
 
-fromtolabel=ct.CTkLabel(root,  text='write nodes which will be the start and the end in format: "A B"')
+fromtolabel=ct.CTkLabel(root,  text='write nodes which will be the start and the end in format: "1 2"')
 entryoffromto = ct.CTkEntry(root)
 fromtolabel.pack(pady=10)
 entryoffromto.pack(pady=20)
 
-namlabel=ct.CTkLabel(root,  text='write names of nodes in format: "A B C ..."')
-entryofnam = ct.CTkEntry(root)
-namlabel.pack(pady=10)
-entryofnam.pack(pady=20)
 
 
-def go():
+
+def goD():
     fromto = entryoffromto.get().split()
     start = int(fromto[0])
     end = int(fromto[1])
-    graph = PreparationForVisualization(test_graph, entryoftype.get(), entryofnam.get().split(), function=entryoffunction.get())
-    graph.VisualisationOfShortestWay(start, end)
+    graph = PreparationForVisualization(test_graph, entryoftype.get(), function=entryoffunction.get())
+    graph.VisualisationOfShortestWayD(start, end)
+
+def goFW():
+    fromto = entryoffromto.get().split()
+    start = int(fromto[0])
+    end = int(fromto[1])
+    graph = PreparationForVisualization(test_graph, entryoftype.get(), function=entryoffunction.get())
+    graph.VisualisationOfShortestWayFW(start, end)
+
+def goFB():
+    fromto = entryoffromto.get().split()
+    start = int(fromto[0])
+    end = int(fromto[1])
+    graph = PreparationForVisualization(test_graph, entryoftype.get(), function=entryoffunction.get())
+    graph.VisualisationOfShortestWayFB(start, end)
 
 
-button = ct.CTkButton(root, text='ready', command=go)
-button.pack()
+buttonD = ct.CTkButton(root, text='ready D', command=goD)
+buttonFW = ct.CTkButton(root, text='ready FW', command=goFW)
+buttonFB = ct.CTkButton(root, text='ready FB', command=goFB)
+buttonD.pack()
+buttonFW.pack()
+buttonFB.pack()
 
+
+
+#def newnodef():
+
+
+
+
+#newnode = ct.CTkLabel(root, text='write new node')
+#newnodew = ct.CTkEntry(root)
+#nww = ct.CTkButton(root, text='ready', command=newnodef)
+#newnode.pack(pady=10)
+#newnodew.pack(pady=10)
+#nww.pack(pady=10)
+
+
+#def newedgef():
+
+
+
+
+#newedgew = ct.CTkLabel(root, text='write new edge')
+#newedgee = ct.CTkEntry(root)
+#nwe = ct.CTkButton(root, text='ready', command=newedgef)
+#newedgew.pack(pady=10)
+#newedgee.pack(pady=10)
+#nwe.pack(pady=10)
 
 
 test_graph = [
@@ -69,3 +109,5 @@ test_graph = [
 
 
 root.mainloop()
+
+
